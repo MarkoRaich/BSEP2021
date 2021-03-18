a8ea45fe7a8df93464928c71f988c0930305e46a
MarkoRaich <33671965+MarkoRaich@users.noreply.github.com>
2021-03-18 19:23:34 +0100

    Inicijalno kreiranje
    Inicijalno kreiranje serverske i klijentske aplikacije i povezivanje sa github repozitorijumom

---
+++ backend/src/test/java/ftn/bsep/PkiApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.bsep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
